b21f7fdc2a8d018f991e2af614412df2616795fd
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-01 23:01:26 +0500

    save

--- src/HRMS.Application/UseCases/TimeSheets/Commands/UpdateTimeSheet/UpdateTimeSheetCommand.cs
+++ src/HRMS.Application/UseCases/TimeSheets/Commands/UpdateTimeSheet/UpdateTimeSheetCommand.cs
@@ -3,10 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using HRMS.Application.Common.Exceptions;
+using HRMS.Application.Common.Interfaces;
+using HRMS.Application.UseCases.TimeSheets.Models;
+using HRMS.Domain.Entities.Employees;
+using HRMS.Domain.Entities.TimeSheets;
+using MediatR;
 
 namespace HRMS.Application.UseCases.TimeSheets.Commands.UpdateTimeSheet
 {
-    internal class UpdateTimeSheetCommand
+    public class UpdateTimeSheetCommand:IRequest<TimeSheetDto>
     {
+        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
+        public DateTimeOffset WorkingDay { get; set; }
+        public double WorkedHours { get; set; }
+    }
+
+    public class UpdateTimeSheetCommandHandler : IRequestHandler<UpdateTimeSheetCommand, TimeSheetDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UpdateTimeSheetCommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        async Task<TimeSheetDto> IRequestHandler<UpdateTimeSheetCommand, TimeSheetDto>.Handle(UpdateTimeSheetCommand request, CancellationToken cancellationToken)
+        {
+            TimeSheet maybeTimeSheet = await
+                _context.TimeSheets.FindAsync(new object[] { request.Id });
+
+            ValidateTimeSheetDtoIsNotNull(request, maybeTimeSheet);
+
+            Employee maybeEmployee = _context.Employees
+                .SingleOrDefault(p => p.Id.Equals(request.EmployeeId));
+
+            ValidateEmployeeAreNotNull(request, maybeEmployee);
+
+            maybeTimeSheet.WorkedHours=request.WorkedHours;
+            maybeTimeSheet.WorkingDay=request.WorkingDay;
+            maybeTimeSheet.Employee = maybeEmployee;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return _mapper.Map<TimeSheetDto>(maybeTimeSheet);
+        }
+
+        private static void ValidateEmployeeAreNotNull(UpdateTimeSheetCommand request, Employee? maybeEmployee)
+        {
+            if (maybeEmployee is null)
+            {
+                throw new NotFoundException(nameof(Employee), request.EmployeeId);
+            }
+        }
+
+        private static void ValidateTimeSheetDtoIsNotNull(UpdateTimeSheetCommand request, TimeSheet maybeTimeSheet)
+        {
+            if (maybeTimeSheet == null)
+            {
+                throw new NotFoundException(nameof(TimeSheet), request.Id);
+            }
+        }
     }
 }
--- src/HRMS.Application/UseCases/TimeSheets/Commands/UpdateTimeSheet/UpdateTimeSheetCommandValidator.cs
+++ src/HRMS.Application/UseCases/TimeSheets/Commands/UpdateTimeSheet/UpdateTimeSheetCommandValidator.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace HRMS.Application.UseCases.TimeSheets.Commands.UpdateTimeSheet
 {
-    internal class UpdateTimeSheetCommandValidator
+    public class UpdateTimeSheetCommandValidator:AbstractValidator<UpdateTimeSheetCommand>
     {
+        public UpdateTimeSheetCommandValidator()
+        {
+            RuleFor(t => t.Id).NotEmpty()
+                .NotEqual((Guid)default)
+                .WithMessage("Id is required.");
+
+            RuleFor(t => t.EmployeeId).NotEmpty()
+                .NotEqual((Guid)default)
+                .WithMessage("Employee id is required.");
+
+            RuleFor(t => t.WorkedHours).NotEqual((double)default)
+                .GreaterThan(0).WithMessage("Worked Hours is required.");
+
+            RuleFor(t => t.WorkingDay).NotEqual((DateTimeOffset)default)
+                .WithMessage("Working date is required.");
+        }
     }
 }
