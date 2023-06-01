1ff6d3ae2e25824a5be1e4b40f97dfd46eea60ef
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-02 00:34:51 +0500

    save

---
+++ src/HRMS.Application/ConfigureService.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using FluentValidation;
+using HRMS.Application.Common.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRMS.Application
+{
+    public static class ConfigureService
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+                config.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+                config.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            });
+
+            return services;
+        }
+    }
+}
