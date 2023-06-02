26014ae082ba04ae71ace9b8742fbbc83f7b0b41
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-02 18:40:04 +0500

    save

--- src/HRMS.Application/UseCases/Positions/Commands/UpdatePosition/UpdatePositionCommandValidator.cs
+++ src/HRMS.Application/UseCases/Positions/Commands/UpdatePosition/UpdatePositionCommandValidator.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace HRMS.Application.UseCases.Positions.Commands.UpdatePosition
 {
-    internal class UpdatePositionCommandValidator
+    public class UpdatePositionCommandValidator:AbstractValidator<UpdatePositionCommand>
     {
+        public UpdatePositionCommandValidator()
+        {
+            RuleFor(t => t.Id).NotEmpty()
+          .NotEqual((Guid)default)
+          .WithMessage("Id is required.");
+
+            RuleFor(t => t.DepartmentId).NotEmpty()
+                .NotEqual((Guid)default)
+                .WithMessage("Department id is required.");
+
+            RuleFor(t => t.SalaryId).NotEmpty()
+                .NotEqual((Guid)default)
+                .WithMessage("Salary id is required.");
+
+            RuleFor(p => p.Name)
+               .NotEmpty()
+               .MaximumLength(50)
+               .WithMessage("Position name is required.");
+
+        }
     }
 }
---
+++ src/HRMS.Application/UseCases/Positions/Queries/GetPositions/GetPositionsQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HRMS.Application.Common.Interfaces;
+using HRMS.Application.UseCases.Positions.Models;
+using HRMS.Domain.Entities.Positions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMS.Application.UseCases.Positions.Queries.GetPositions
+{
+    public record GetPositionsQuery : IRequest<PositionDto[]>;
+
+    public class GetPositionsQueryHandler : IRequestHandler<GetPositionsQuery, PositionDto[]>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetPositionsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PositionDto[]> Handle(GetPositionsQuery request, CancellationToken cancellationToken)
+        {
+            Position[] Positions = await _context.Positions.ToArrayAsync();
+
+            return _mapper.Map<PositionDto[]>(Positions);
+        }
+    }
+}
