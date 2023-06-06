b32afc91135c17841d4f4416b89dc1a79b819c63
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-06 07:17:59 +0500

    save

---
+++ src/HRMS.Application/UseCases/Departments/Notifications/DepartmentCreatedNotification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Serilog;
+
+namespace HRMS.Application.UseCases.Departments.Notifications
+{
+    public record DepartmentCreatedNotification(string name) : INotification;
+
+    public class DepartmentCreatedLogNotificationHandler : INotificationHandler<DepartmentCreatedNotification>
+    {
+        public Task Handle(DepartmentCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            Log.Information($"HRMS: New department created with {notification.name} name.");
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class DepartmentCreatedConsoleNotificationHandler : INotificationHandler<DepartmentCreatedNotification>
+    {
+        public async Task Handle(DepartmentCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            await Console.Out.WriteLineAsync($"HRMS: New department created with {notification.name} name.");
+        }
+    }
+}
---
+++ src/HRMS.Application/UseCases/Departments/Notifications/DepartmentUpdatedNotification.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRMS.Domain.Entities.Departments;
+using MediatR;
+using Serilog;
+
+namespace HRMS.Application.UseCases.Departments.Notifications
+{
+
+    public class DepartmentUpdatedNotification : INotification
+    {
+        public Department CurrentDepartment { get; set; }
+        public Department UpdatedDepartment { get; set; }
+    }
+
+    public class DepartmentUpdatedNotificationHandler : INotificationHandler<DepartmentUpdatedNotification>
+    {
+        public Task Handle(DepartmentUpdatedNotification notification, CancellationToken cancellationToken)
+        {
+
+            Log.Information($"HRMS: Update department notification\nCurrent department: {notification.CurrentDepartment}\n" +
+                $"Updated department: {notification.UpdatedDepartment}");
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
