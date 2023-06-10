88af741d25bd41eb5e1b0d3440ef69201f1b01b9
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-10 10:45:55 +0500

    save

--- src/HRMS.Infrastructure/Persistence/Interceptors/AuditableEntitySaveChangesInterceptor.cs
+++ src/HRMS.Infrastructure/Persistence/Interceptors/AuditableEntitySaveChangesInterceptor.cs
@@ -19,14 +19,18 @@ namespace HRMS.Infrastructure.Persistence.Interceptors
             _guidGenerator = guidGenerator;
         }
 
-        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        public override InterceptionResult<int> SavingChanges(
+            DbContextEventData eventData, 
+            InterceptionResult<int> result)
         {
             UpdateEntities(eventData.Context);
 
             return base.SavingChanges(eventData, result);
         }
 
-        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData, InterceptionResult<int> result, 
+            CancellationToken cancellationToken = default)
         {
             UpdateEntities(eventData.Context);
 
